4298dc564ae57d0eb33e7b0dba4566e5fff208ad
pevlik <101806933+pevlik@users.noreply.github.com>
2023-12-26 16:13:57 +0300

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-# import json
-
-# def load_data():
-#   with open('data.json') as f:
-#     return json.load(f)
-
-# def save_data(data):
-#   with open('data.json', 'w') as f:
-#      json.dump(data, f)
